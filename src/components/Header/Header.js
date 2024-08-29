c81cf694d3b033f83b714fbbed50089be1f90ee5
Rayane Reis <rayanereismota2@gmail.com>
2024-08-29 09:20:51 -0300

    Converter header para não utilizar mais react-hash-link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,16 +3,17 @@ import "./Header.css"
 import {Link} from 'react-router-dom';
 import logo from 'image/logo.png'
 
+
 function Header(props) {
     return (
         <>
             <div className={`${props.theme} header`}>
-                {/* <HashLink to="/#"><img className='logoheader' src={logo} alt="logo" id="logo-header"/></HashLink> */}
+                <Link to="/"><img className='logoheader' src={logo} alt="logo" id="logo-header"/></Link>
                 <ul className='list'>
                     <li className='item'><Link to="/">Home</Link></li>
                     <li className='item'><Link to="/sobre">Quem Somos</Link></li>
                     <li className='item'><Link to="/portfolio">Portfolio</Link></li>
-                    {/* <li className='item'><HashLink to="/#agendar-reuniao">Contato</HashLink></li> */}
+                    <li className='item'><Link to="/#agendar-reuniao">Contato</Link></li>
                 </ul>
             </div>
         </>
