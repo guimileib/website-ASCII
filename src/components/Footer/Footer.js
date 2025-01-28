c553bd593dd04bd8b41d2f999fd83ee0382291e9
Marcus Arthur <marcusarthurl123@gmail.com>
2025-01-28 14:20:14 -0300

    Realização do Rodapé, preciso melhoras a responsividade

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,45 +1,48 @@
 import React from 'react';
-import {Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import logo from 'image/logoHorizontal.png';
 import instagram from 'image/instagram.png';
-import Linkedin from 'image/linkedin.png' 
+import Linkedin from 'image/linkedin.png';
 import Git from 'image/github.png';
 
 import './Footer.css';
 import Background from 'image/mapa-fundo.svg';
+import WaveTop from 'image/wave-top.svg';
 
 function Footer() {
   return (
-      <footer className="footer">
-          <div className='footer__wrapper'
-          style={{backgroundImage: `url(${Background})`}}>
-            <div className='footer__container'>
-              <h3>Nossos Serviços</h3>
-              <ul>
-                <li> <Link to="/#services-header">Criação de Aplicativos</Link></li>
-                <li> <Link to="/#services-header">Desenvolvimento Web</Link></li>
-                <li> <Link to="/#services-header">Modelagem de Sistemas</Link></li>
-                <li> <Link to="/#services-header">Consultoria</Link></li>
-              </ul>
-            </div> 
+    <footer className="footer">
+      {/* Adicionando a onda no topo do footer */}
+      <img className='wave-top' src={WaveTop} alt="Onda superior" />
 
-            <a href="#logo-header"><img src={logo} alt="" className="logo" /></a>
-            <div className='footer__container'>
-              <h3>Informações de Contato</h3> 
-              <ul>
-                <li><Link to="/portfolio/#portfolio-anchor">Portfolio</Link></li>
-                <li><Link to="/sobre/#sobre-anchor">Quem Somos</Link></li>
-                <div className='footer__social-media'>
-                  <Link target="_blank" to="https://www.instagram.com/asciiempresajr/"><img src={instagram} alt="Link para o Instagram" className="instagram-icon" /></Link>
-                  <Link target="_blank" to="https://www.linkedin.com/company/ascii-empresa-j%C3%BAnior/"><img src={Linkedin} alt="Link para o Linkedin" className="linkedin-icon" /></Link>
-                  <Link target="_blank" to="https://github.com/asciiej"><img src={Git} alt="Link para o Github" className="github-icon" /></Link>
-                </div>
-              </ul>
+      <div className='footer__wrapper' style={{ backgroundImage: `url(${Background})` }}>
+        <div className='footer__container'>
+          <h3>Nossos Serviços</h3>
+          <ul>
+            <li> <Link to="/#services-header">Criação de Aplicativos</Link></li>
+            <li> <Link to="/#services-header">Desenvolvimento Web</Link></li>
+            <li> <Link to="/#services-header">Modelagem de Sistemas</Link></li>
+            <li> <Link to="/#services-header">Consultoria</Link></li>
+          </ul>
+        </div>
+
+        <a href="#logo-header"><img src={logo} alt="" className="logo" /></a>
+        <div className='footer__container'>
+          <h3>Informações de Contato</h3>
+          <ul>
+            <li><Link to="/portfolio/#portfolio-anchor">Portfolio</Link></li>
+            <li><Link to="/sobre/#sobre-anchor">Quem Somos</Link></li>
+            <div className='footer__social-media'>
+              <Link target="_blank" to="https://www.instagram.com/asciiempresajr/"><img src={instagram} alt="Link para o Instagram" className="instagram-icon" /></Link>
+              <Link target="_blank" to="https://www.linkedin.com/company/ascii-empresa-j%C3%BAnior/"><img src={Linkedin} alt="Link para o Linkedin" className="linkedin-icon" /></Link>
+              <Link target="_blank" to="https://github.com/asciiej"><img src={Git} alt="Link para o Github" className="github-icon" /></Link>
             </div>
-          </div> 
-          <p className="copyright">© 2024 ASCII</p>
-      </footer>
+          </ul>
+        </div>
+      </div>
+      <p className="copyright">© 2024 ASCII</p>
+    </footer>
   );
 }
 
