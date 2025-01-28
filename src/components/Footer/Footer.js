6110abaa1f1950c7758cb79d9f84cd48a2732730
Marcus Arthur <marcusarthurl123@gmail.com>
2025-01-28 14:47:13 -0300

    Realização do Footer, falta só deixar responsivo.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { Link, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 
 import logo from 'image/logoHorizontal.png';
-import instagram from 'image/instagram.png';
-import Linkedin from 'image/linkedin.png';
-import Git from 'image/github.png';
 
 import './Footer.css';
 import Background from 'image/mapa-fundo.svg';
@@ -33,15 +30,12 @@ function Footer() {
           <ul>
             <li><Link to="/portfolio/#portfolio-anchor">Portfolio</Link></li>
             <li><Link to="/sobre/#sobre-anchor">Quem Somos</Link></li>
-            <div className='footer__social-media'>
-              <Link target="_blank" to="https://www.instagram.com/asciiempresajr/"><img src={instagram} alt="Link para o Instagram" className="instagram-icon" /></Link>
-              <Link target="_blank" to="https://www.linkedin.com/company/ascii-empresa-j%C3%BAnior/"><img src={Linkedin} alt="Link para o Linkedin" className="linkedin-icon" /></Link>
-              <Link target="_blank" to="https://github.com/asciiej"><img src={Git} alt="Link para o Github" className="github-icon" /></Link>
-            </div>
+            <li><Link to="/explore">Explore+</Link></li>
+            <li><Link to="/contato">Contato</Link></li>
           </ul>
         </div>
       </div>
-      <p className="copyright">© 2024 ASCII</p>
+      <p className="copyright" style={{ marginTop: '-20px' }}>&copy; 2024 ASCII</p>
     </footer>
   );
 }
