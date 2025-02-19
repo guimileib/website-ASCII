c0b9fb5e2b3b58688e388dc92538f605f2085d78
Malcon Rezende Rodrigues <114543246+MalconUFU@users.noreply.github.com>
2025-02-18 21:06:23 -0300

    Update ButtonWhatsapp.js

--- src/components/ButtonWhatsapp/ButtonWhatsapp.js
+++ src/components/ButtonWhatsapp/ButtonWhatsapp.js
@@ -6,7 +6,7 @@ import whatsappImagem from "../../image/botaoWhatsapp1.png";
 function ButtonWhatsapp(){
     return(
         <>
-            <a href="https://api.whatsapp.com/send?phone=5511971669926" target="_black" className="botaoWhatsapp">
+            <a href="https://wa.me/+5534991523387" target="_black" className="botaoWhatsapp">
                 <img src={whatsappImagem} alt="Botão Whatsapp"/>
             </a>
         </>
